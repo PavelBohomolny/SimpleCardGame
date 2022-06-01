c7b2f9bb07a24375bf93c69f031a30f9de93fdb9
PavelBohomolny <pbogomolny93@gmail.com>
2022-06-01 12:02:52 +0200

    finished project

---
+++ SimpleCardGame/SimpleCardGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleCardGameApp.swift
+//  SimpleCardGame
+//
+//  Created by Pavel Bohomolnyi on 01/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct SimpleCardGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
